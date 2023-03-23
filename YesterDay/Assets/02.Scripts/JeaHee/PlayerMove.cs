5140089e92a6f7208566850522cf85ad9dd77514
ahslxjvhapt12 <likjhee1@gmail.com>
2023-03-23 18:50:37 +0900

    Rotate
    Rotate : Cam, Player

--- YesterDay/Assets/02.Scripts/JeaHee/PlayerMove.cs
+++ YesterDay/Assets/02.Scripts/JeaHee/PlayerMove.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerMove : MonoBehaviour
 {
@@ -8,11 +10,16 @@ public class PlayerMove : MonoBehaviour
     private float speed;      // 캐릭터 움직임 스피드.
     private float jumpSpeed;  // 캐릭터 점프 힘.
     private float gravity;    // 캐릭터에게 작용하는 중력.
+    private float rotateSpeed = 50f;
 
+    private float mouseX = 0;
+
+
+    private Camera _cam;
     private CharacterController controller; // 현재 캐릭터가 가지고있는 캐릭터 컨트롤러 콜라이더.
     private Vector3 MoveDir;                // 캐릭터의 움직이는 방향.
 
-    void Start()
+    void Awake()
     {
         speed = 6.0f;
         jumpSpeed = 8.0f;
@@ -20,11 +27,19 @@ public class PlayerMove : MonoBehaviour
 
         MoveDir = Vector3.zero;
         controller = GetComponent<CharacterController>();
+        _cam = Camera.main;
     }
 
     void Update()
     {
+        //    mouseX += Input.GetAxis("Mouse X");
+        //    transform.eulerAngles = new Vector3(0, mouseX, 0);
+
+        //float h = Input.GetAxisRaw("Horizontal");
+        //transform.rotation = Quaternion.Euler(0, h * Time.deltaTime * rotateSpeed, 0) * transform.rotation;
         // 현재 캐릭터가 땅에 있는가?
+
+        transform.eulerAngles = new Vector3(0, _cam.transform.eulerAngles.y, 0);
         if (controller.isGrounded)
         {
             MoveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
