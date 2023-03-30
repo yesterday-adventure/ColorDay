c0d8062ea2744f02700fe8e15a51ddf5d1f5cf3e
준이 <98876187+June060522@users.noreply.github.com>
2023-03-30 19:13:00 +0900

    ah mola commit halle

---
+++ YesterDay/Assets/02.Scripts/June/PlayerRotation.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour
+{
+    public float rotateSpeed = 10f;
+
+    Vector3 mousePos;
+    private void FixedUpdate()
+    {
+        
+    }
+}
