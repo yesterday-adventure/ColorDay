443913a9aa4cb490c4dd2e3328742a3d089e6acd
CAFUCH <tjfdk6201@gmail.com>
2023-03-30 18:25:55 +0900

    발사 스왑 완성

--- YesterDay/Assets/02.Scripts/cafuch/BulletSwap.cs
+++ YesterDay/Assets/02.Scripts/cafuch/BulletSwap.cs
@@ -4,11 +4,7 @@ using UnityEngine;
 
 public class BulletSwap : MonoBehaviour
 {
-    //red = 1 2
-    //blue = 6 0 
-    //yellow = 10 1
-
-    public int idx = 9;
+    public int idx = 3;
     public ColorEnum colorEnum = ColorEnum.red;
     private void Update() {
         
@@ -17,21 +13,19 @@ public class BulletSwap : MonoBehaviour
 
     private void Swap()
     {
-        if (Input.GetKeyDown(KeyCode.Q)) 
+        if (Input.GetKeyDown(KeyCode.E)) 
         {
             idx++;
-            if(idx == 0)
-                idx = 9;
         }
-        else if (Input.GetKeyDown(KeyCode.E)) 
+        else if (Input.GetKeyDown(KeyCode.Q)) 
         {
             idx--;
+            if(idx == 0)
+                idx = 3;
         }
 
         if (idx % 3 == 0) colorEnum = ColorEnum.red;
         else if (idx % 3 == 1) colorEnum = ColorEnum.yellow;
         else colorEnum = ColorEnum.blue;
     }
-
-    //0 예외처리
 }
