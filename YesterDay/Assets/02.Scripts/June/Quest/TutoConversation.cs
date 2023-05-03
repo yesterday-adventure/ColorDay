0a3e1601b4865ae2dcf013a08c1ad69a8d119b05
준이 <98876187+June060522@users.noreply.github.com>
2023-05-03 18:55:38 +0900

    a

--- YesterDay/Assets/02.Scripts/June/Quest/TutoConversation.cs
+++ YesterDay/Assets/02.Scripts/June/Quest/TutoConversation.cs
@@ -2,14 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
 
 public class TutoConversation : MonoBehaviour
 {
     public static TutoConversation Instance;
 
     [SerializeField] TextMeshProUGUI slimeTxt;
+    [SerializeField] Image image;
+    [SerializeField] Sprite slime;
+    [SerializeField] Sprite user;
     public int idx = 0;
 
+    Text text;
     public string[] conversationList;
 
     private void Awake()
@@ -18,5 +24,28 @@ public class TutoConversation : MonoBehaviour
             Instance = this;
         else
             Debug.LogError("TutoConversation is Multiple");
+    }
+
+    private void Start()
+    {
+        Text();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            idx++;
+            Text();
+        }
+    }
+
+    public void Text()
+    {
+        if(idx % 2 == 0) 
+            image.sprite = slime;
+        if (idx % 2 == 1)
+            image.sprite = user;
+        slimeTxt.text = conversationList[idx];
     }
 }
