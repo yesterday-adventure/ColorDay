1b36002497877fc7734d20e4e31aa9e4e2c6d342
CAFUCH <tjfdk6201@gmail.com>
2023-05-07 16:30:35 +0900

    fsm
    attact도 되긴햇는데 응 모르겠고
    로그는 찍힘

--- YesterDay/Assets/02.Scripts/cafuch/FSM/Actions/AttactAction.cs
+++ YesterDay/Assets/02.Scripts/cafuch/FSM/Actions/AttactAction.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IdleAction : AIAction
+public class AttactAction : AIAction
 {
     public override void TakeAction()
     {
-        _aiBrain.SetDestinationF(_aiBrain._playerTrm.position, 0f);
+        _aiBrain.SetDestinationF(Vector3.zero, 0f);
+        Debug.Log("죽어라 시발");
     }
 }
